66e67126d24641d2af84229bd142c9bc3e66083a
masrafihaque <masrafigaming1357@gmail.com>
2026-03-05 22:34:52 +0600

    first commit

---
+++ js/home.js
@@ -0,0 +1,5 @@
+let todolistbutton = document.querySelector(".todolist");
+
+todolistbutton.addEventListener("click", () =>{
+    window.location.href = "../html/todolist.html"
+})
---
+++ js/todolist.js
@@ -0,0 +1,5 @@
+let todolistbutton = document.querySelector(".dashboard");
+
+todolistbutton.addEventListener("click", () =>{
+    window.location.href = "../html/home.html"
+})
